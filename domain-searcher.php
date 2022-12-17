bd4eab7cf8a78d459cc110d437ee18dc91d31b1d
Laurence Bahiirwa <laurence@mediauganda.com>
2022-12-17 11:43:33 +0300

    Initial setup

---
+++ domain-searcher.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: TechiePress Domain Searcher
+ * Plugin URI:  https://omukiguy.com
+ * Author:      TechiePress
+ * Author URI:  https://omukiguy.com
+ * Description: Enables the search, renewal and registration of UG domains.
+ * Version:     0.1.0
+ * License:     GPL-2.0+
+ * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: techiepress-domain-searcher
+*/
+
+defined( 'ABSPATH' ) or die( 'Unauthorize access!' );
