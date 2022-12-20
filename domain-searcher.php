5499fd36af7195e982634095af4684f6191b95c0
Laurence Bahiirwa <laurence@mediauganda.com>
2022-12-20 08:35:29 +0300

    query xml + debug onscreen and errors

--- domain-searcher.php
+++ domain-searcher.php
@@ -20,5 +20,43 @@ function add_input_form() {
                 <input type="text" value="" name="domain-searcher-input" class="domain-searcher-input" />
                 <input type="submit" value="Search" class="domain-searcher-submit" />
             </form>';
+
+    check_availability();
+
     return $form;
+}
+
+function check_availability() {
+
+    $body = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <request cmd="check">
+        <domains>
+            <domain name="ura.go.ug"></domain>
+            <domain name="techiepress.ug"></domain>
+        </domains>
+    </request>';
+
+    $url  = 'https://registry.co.ug/api';
+    $args = array(
+        'method'  => 'POST',
+        'body'    => $body,
+        'headers' => array(
+            'Content-Type' => 'application/xml'
+        ),
+    );
+
+    $results = wp_remote_post( $url, $args );
+
+    $body_response = wp_remote_retrieve_body($results);
+
+    // error_log( print_r( $body_response, true ) );
+
+    $xml = simplexml_load_string( $body_response );
+    $json = json_encode($xml);
+    $php = json_decode($json);
+
+    echo '<pre>';
+    var_dump( $php);
+    echo '</pre>';
+
 }
